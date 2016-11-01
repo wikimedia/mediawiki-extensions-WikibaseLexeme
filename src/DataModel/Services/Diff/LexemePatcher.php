d130aa846954a4b128e7248d9c1de1d0a859fb2a
Amir Sarabadani <Ladsgroup@gmail.com>
2016-11-01 16:17:44 +0330

    Add LexemePatcher
    Bug: T149497
    Change-Id: Ibf6f593378cc7aaa7c4e9c7c4be42beee98fbbaf

--- src/DataModel/Services/Diff/LexemePatcher.php
+++ src/DataModel/Services/Diff/LexemePatcher.php
@@ -6,14 +6,55 @@ use InvalidArgumentException;
 use Wikibase\DataModel\Entity\EntityDocument;
 use Wikibase\DataModel\Services\Diff\EntityDiff;
 use Wikibase\DataModel\Services\Diff\EntityPatcherStrategy;
+use Wikibase\DataModel\Services\Diff\StatementListPatcher;
+use Wikibase\DataModel\Services\Diff\TermListPatcher;
+use Wikibase\Lexeme\DataModel\Lexeme;
+use Wikimedia\Assert\Assert;
 
+/**
+ * @license GPL-2.0+
+ * @author Amir Sarabadani <ladsgroup@gmail.com>
+ */
 class LexemePatcher implements EntityPatcherStrategy {
 
-	public function canPatchEntityType( $entityType ) {
-		// TODO: Implement canPatchEntityType() method.
+	/**
+	 * @var TermListPatcher
+	 */
+	private $termListPatcher;
+
+	/**
+	 * @var StatementListPatcher
+	 */
+	private $statementListPatcher;
+
+	public function __construct() {
+		$this->termListPatcher = new TermListPatcher();
+		$this->statementListPatcher = new StatementListPatcher();
 	}
 
-	public function patchEntity( EntityDocument $entity, EntityDiff $patch ) {
-		// TODO: Implement patchEntity() method.
+	/**
+	 * @param string $entityType
+	 *
+	 * @return bool
+	 */
+	public function canPatchEntityType( $entityType ) {
+		return $entityType === Lexeme::ENTITY_TYPE;
 	}
+
+	/**
+	 * @param EntityDocument $entity
+	 * @param EntityDiff $patch
+	 *
+	 * @throws InvalidArgumentException
+	 */
+	public function patchEntity( EntityDocument $entity, EntityDiff $patch ) {
+
+		Assert::parameterType( Lexeme::class, $entity, '$entity' );
+
+		$this->statementListPatcher->patchStatementList(
+			$entity->getStatements(),
+			$patch->getClaimsDiff()
+		);
+	}
+
 }
---
+++ tests/phpunit/composer/DataModel/Services/Diff/LexemePatcherTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Wikibase\Lexeme\Tests\DataModel\Services\Diff;
+
+use Diff\DiffOp\Diff\Diff;
+use Diff\DiffOp\DiffOpAdd;
+use Diff\DiffOp\DiffOpRemove;
+use InvalidArgumentException;
+use PHPUnit_Framework_TestCase;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Services\Diff\EntityDiff;
+use Wikibase\DataModel\Snak\PropertyNoValueSnak;
+use Wikibase\DataModel\Statement\Statement;
+use Wikibase\Lexeme\DataModel\Lexeme;
+use Wikibase\Lexeme\DataModel\Services\Diff\LexemePatcher;
+
+/**
+ * @covers Wikibase\Lexeme\DataModel\Services\Diff\LexemePatcher
+ *
+ * @group WikibaseLexeme
+ *
+ * @license GPL-2.0+
+ * @author Amir Sarabadani <ladsgroup@gmail.com>
+ */
+class LexemePatcherTest extends PHPUnit_Framework_TestCase {
+
+	public function testGivenEmptyDiff_lexemeIsReturnedAsIs() {
+		$lexeme = new Lexeme();
+
+		$patched = $lexeme->copy();
+		$patcher = new LexemePatcher();
+		$patcher->patchEntity( $patched, new EntityDiff() );
+
+		$this->assertInstanceOf( Lexeme::class, $patched );
+		$this->assertTrue( $lexeme->equals( $patched ) );
+	}
+
+	public function testCanPatchEntityType() {
+		$patcher = new LexemePatcher();
+		$this->assertTrue( $patcher->canPatchEntityType( 'lexeme' ) );
+		$this->assertFalse( $patcher->canPatchEntityType( 'property' ) );
+		$this->assertFalse( $patcher->canPatchEntityType( '' ) );
+		$this->assertFalse( $patcher->canPatchEntityType( null ) );
+	}
+
+	public function testGivenNonLexeme_exceptionIsThrown() {
+		$patcher = new LexemePatcher();
+
+		$this->setExpectedException( InvalidArgumentException::class );
+		$patcher->patchEntity( new Item(), new EntityDiff() );
+	}
+
+	public function testStatementsArePatched() {
+		$removedStatement = new Statement( new PropertyNoValueSnak( 1 ), null, null, 's1' );
+		$addedStatement = new Statement( new PropertyNoValueSnak( 2 ), null, null, 's2' );
+
+		$lexeme = new Lexeme();
+		$lexeme->getStatements()->addStatement( $removedStatement );
+
+		$patch = new EntityDiff( [
+			'claim' => new Diff( [
+				's1' => new DiffOpRemove( $removedStatement ),
+				's2' => new DiffOpAdd( $addedStatement ),
+			] ),
+		] );
+
+		$expected = new Lexeme();
+		$expected->getStatements()->addStatement( $addedStatement );
+
+		$patcher = new LexemePatcher();
+		$patcher->patchEntity( $lexeme, $patch );
+		$this->assertEquals( $expected->getStatements(), $lexeme->getStatements() );
+		$this->assertTrue( $expected->equals( $lexeme ) );
+	}
+
+}
