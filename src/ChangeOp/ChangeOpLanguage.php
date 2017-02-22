a4be0caf2cdfffca4bcdd468d58a8d75eb66cc4e
Amir Sarabadani <Ladsgroup@gmail.com>
2017-02-22 21:12:08 +0330

    Add ChangeOpLanguage
    For now, it's using NullValidator.
    
    Bug: T155701
    Change-Id: I8d1287650e530067210524cab057e40a000a6c66

---
+++ src/ChangeOp/ChangeOpLanguage.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Wikibase\Lexeme\ChangeOp;
+
+use InvalidArgumentException;
+use ValueValidators\Result;
+use Wikibase\ChangeOp\ChangeOpBase;
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\Lexeme\DataModel\Lexeme;
+use Wikibase\Lexeme\DataModel\Providers\LanguageProvider;
+use Wikibase\Lexeme\Validators\LexemeValidatorFactory;
+use Wikibase\Summary;
+use Wikimedia\Assert\Assert;
+
+/**
+ * @license GPL-2.0+
+ */
+class ChangeOpLanguage extends ChangeOpBase {
+
+	/**
+	 * @var ItemId|null
+	 */
+	private $language;
+
+	/**
+	 * @var LexemeValidatorFactory
+	 */
+	private $lexemeValidatorFactory;
+
+	/**
+	 * @param ItemId|null $language
+	 * @param LexemeValidatorFactory $lexemeValidatorFactory
+	 *
+	 * @throws InvalidArgumentException
+	 */
+	public function __construct( $language, LexemeValidatorFactory $lexemeValidatorFactory ) {
+		if ( $language !== null ) {
+			Assert::parameterType( ItemId::class, $language, '$language' );
+		}
+
+		$this->language = $language;
+		$this->lexemeValidatorFactory = $lexemeValidatorFactory;
+	}
+
+	/**
+	 * @param EntityDocument $entity
+	 *
+	 * @return Result
+	 *
+	 * @throws InvalidArgumentException
+	 */
+	public function validate( EntityDocument $entity ) {
+		Assert::parameterType( LanguageProvider::class, $entity, '$entity' );
+
+		$languageValidator = $this->lexemeValidatorFactory->getLanguageValidator();
+
+		return $languageValidator->validate( $this->language );
+	}
+
+	/**
+	 * @param EntityDocument $entity
+	 * @param Summary|null $summary
+	 *
+	 * @throws InvalidArgumentException
+	 */
+	public function apply( EntityDocument $entity, Summary $summary = null ) {
+		Assert::parameterType( Lexeme::class, $entity, '$entity' );
+
+		/** @var Lexeme $entity */
+		// TODO: Add setLanguage to LanguageProvider interface
+		$language = $entity->getLanguage();
+
+		if ( $this->language === null ) {
+			if ( $language ) {
+				$this->updateSummary(
+					$summary,
+					'remove',
+					'',
+					$language->getSerialization()
+				);
+				$entity->setLanguage( null );
+			}
+
+			return;
+		}
+
+		$this->updateSummary( $summary, 'set', '', $this->language->getSerialization() );
+		$entity->setLanguage( $this->language );
+	}
+
+}
