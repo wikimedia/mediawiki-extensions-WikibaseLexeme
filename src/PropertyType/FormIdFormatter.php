7d7814fec2037a1d2525db2959d627f55dd74239
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2017-07-13 19:56:43 +0200

    FormId property type with demo data
    Didn't want to introduce extra complexity, so just put test data in both
    Form.js and FormIdFormatter.php
    If you will need to add something don't forget that you need to add in both.
    
    Bug: T168373
    Change-Id: Ie131f174175fa7b97bdbbc1f2af7eadf25f71753

---
+++ src/PropertyType/FormIdFormatter.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Wikibase\Lexeme\PropertyType;
+
+use DataValues\StringValue;
+use ValueFormatters\ValueFormatter;
+
+class FormIdFormatter implements ValueFormatter {
+
+	/**
+	 * @var array[]
+	 */
+	private $forms;
+
+	public function __construct() {
+		//Basically, copy-paste of resources/experts/Form.js:5-39
+		//If you change something here, change also there
+		$this->addForm( 'L13', 'hard English adjective', 'F1', 'hard', [ 'normative' ] );
+		$this->addForm( 'L13', 'hard English adjective', 'F2', 'harder', [ 'comparative' ] );
+		$this->addForm( 'L456', 'card English noun', 'F4', 'card', [ 'normative' ] );
+		$this->addForm( 'L888', 'bard English noun', 'F1', 'bard', [ 'normative' ] );
+		$this->addForm(
+			'L14',
+			'Leiter German noun',
+			'F1',
+			'Leiter',
+			[ 'nominative', 'singular' ]
+		);
+		$this->addForm(
+			'L14',
+			'Leiter German noun',
+			'F2',
+			'Leiters',
+			[ 'genitive', 'singular' ]
+		);
+		$this->addForm(
+			'L14',
+			'Leiter German noun',
+			'F2',
+			'Leiterin',
+			[ 'nominative', 'singular', 'female' ]
+		);
+		$this->addForm(
+			'L17',
+			'ask English noun',
+			'F1',
+			'ask',
+			[]
+		);
+	}
+
+	/**
+	 * @param \DataValues\StringValue $value
+	 * @return string
+	 */
+	public function format( $value ) {
+		$f = $this->findFormInfo( $value );
+
+		if ( !$f ) {
+			return $value->serialize();
+		}
+
+		$title = "({$f['lexemeId']}-{$f['formId']}) {$f['grammaticalFeatures']}";
+		$url = "./Lexeme:{$f['lexemeId']}#{$f['formId']}";
+		$label = "{$f['representataion']} ({$f['lexemeDescription']})";
+
+		return <<<HTML
+<a href="$url" title="$title">$label</a>
+HTML;
+	}
+
+	private function addForm(
+		$lexemeId,
+		$lexemeDescription,
+		$formId,
+		$representataion,
+		array $grammaticalFeatures
+	) {
+		$this->forms[] = [
+			'lexemeId' => $lexemeId,
+			'lexemeDescription' => $lexemeDescription,
+			'formId' => $formId,
+			'representataion' => $representataion,
+			'grammaticalFeatures' => implode( ', ', $grammaticalFeatures ),
+
+		];
+	}
+
+	private function findFormInfo( StringValue $value ) {
+		list( $lexemeId, $formId ) = explode( '-', $value->serialize() );
+
+		foreach ( $this->forms as $form ) {
+			if ( $lexemeId === $form['lexemeId'] && $formId === $form['formId'] ) {
+				return $form;
+			}
+		}
+
+		return null;
+	}
+
+}
