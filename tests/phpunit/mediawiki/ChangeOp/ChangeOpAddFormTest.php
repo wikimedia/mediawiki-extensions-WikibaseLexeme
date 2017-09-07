b71a01588cbd7d7c0fbfa383396b4f689ecf8d16
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2017-09-07 17:56:38 +0200

    Add ChangeOp for Form addition
    Bug: T173742
    Change-Id: I0d0de36364ac4490a6640d68942f19aded3dad31

---
+++ tests/phpunit/mediawiki/ChangeOp/ChangeOpAddFormTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Wikibase\Lexeme\Tests\MediaWiki\ChangeOp;
+
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Term\Term;
+use Wikibase\DataModel\Term\TermList;
+use Wikibase\Lexeme\ChangeOp\ChangeOpAddForm;
+use Wikibase\Lexeme\Tests\DataModel\NewLexeme;
+use Wikibase\Repo\Tests\NewItem;
+use Wikibase\Summary;
+
+class ChangeOpAddFormTest extends \PHPUnit_Framework_TestCase {
+
+	public function test_validateFailsIfProvidedEntityIsNotALexeme() {
+		$changeOpAddForm = new ChangeOpAddForm( new TermList(), [] );
+
+		$this->setExpectedException( \InvalidArgumentException::class );
+		$changeOpAddForm->validate( NewItem::withId( 'Q1' )->build() );
+	}
+
+	public function test_validatePassesIfProvidedEntityIsALexeme() {
+		$changeOpAddForm = new ChangeOpAddForm( new TermList(), [] );
+
+		$result = $changeOpAddForm->validate( NewLexeme::create()->build() );
+
+		$this->assertTrue( $result->isValid() );
+	}
+
+	public function test_applyFailsIfProvidedEntityIsNotALexeme() {
+		$changeOpAddForm = new ChangeOpAddForm( new TermList(), [] );
+
+		$this->setExpectedException( \InvalidArgumentException::class );
+		$changeOpAddForm->apply( NewItem::withId( 'Q1' )->build() );
+	}
+
+	public function test_applyAddsFormIfGivenALexeme() {
+		$representations = new TermList( [ new Term( 'en', 'goat' ) ] );
+		$changeOp = new ChangeOpAddForm( $representations, [ new ItemId( 'Q1' ) ] );
+		$lexeme = NewLexeme::create()->build();
+
+		$changeOp->apply( $lexeme );
+
+		$this->assertEquals( $representations, $lexeme->getForms()[0]->getRepresentations() );
+		$this->assertEquals( [ new ItemId( 'Q1' ) ], $lexeme->getForms()[0]->getGrammaticalFeatures() );
+	}
+
+	public function test_applySetsTheSummary() {
+		$representations = new TermList( [ new Term( 'en', 'goat' ) ] );
+		$changeOp = new ChangeOpAddForm( $representations, [] );
+		$lexeme = NewLexeme::create()->build();
+
+		$summary = new Summary();
+
+		$changeOp->apply( $lexeme, $summary );
+
+		$this->assertEquals( 'add', $summary->getActionName() );
+		$this->assertEquals( [ 'goat' ], $summary->getAutoSummaryArgs() );
+	}
+
+}
