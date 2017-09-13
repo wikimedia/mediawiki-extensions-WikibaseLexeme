0a8f7691a313bdc008a43e5a034d7d61df43daee
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-09-13 13:53:11 +0200

    Add request object of AddForm API action and created it from API params
    Change-Id: Ic593fcd9922f732036d7c5bdda4088887aa64928

---
+++ src/Api/AddFormRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Wikibase\Lexeme\Api;
+
+use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Term\Term;
+use Wikibase\DataModel\Term\TermList;
+use Wikibase\Lexeme\ChangeOp\ChangeOpAddForm;
+use Wikibase\Lexeme\DataModel\LexemeId;
+use Wikimedia\Assert\Assert;
+
+/**
+ * @license GPL-2.0+
+ */
+class AddFormRequest {
+
+	/**
+	 * @var LexemeId
+	 */
+	private $lexemeId;
+
+	/**
+	 * @var TermList
+	 */
+	private $representations;
+
+	/**
+	 * @var ItemId[]
+	 */
+	private $grammaticalFeatures;
+
+	/**
+	 * @param LexemeId $lexemeId
+	 * @param TermList $representations
+	 * @param ItemId[] $grammaticalFeatures
+	 */
+	public function __construct(
+		LexemeId $lexemeId,
+		TermList $representations,
+		array $grammaticalFeatures
+	) {
+		Assert::parameterElementType( ItemId::class, $grammaticalFeatures, '$grammaticalFeatures' );
+		Assert::parameter( !$representations->isEmpty(), '$representations', 'should not be empty' );
+
+		$this->lexemeId = $lexemeId;
+		$this->representations = $representations;
+		$this->grammaticalFeatures = $grammaticalFeatures;
+	}
+
+	/**
+	 * @return ChangeOpAddForm
+	 */
+	public function getChangeOp() {
+		return new ChangeOpAddForm( $this->representations, $this->grammaticalFeatures );
+	}
+
+	/**
+	 * @return LexemeId
+	 */
+	public function getLexemeId() {
+		return $this->lexemeId;
+	}
+
+}
