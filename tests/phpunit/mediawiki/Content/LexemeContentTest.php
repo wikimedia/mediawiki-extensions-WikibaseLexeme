dd17a65c1b23ec3076c2a8343468aa1209b3c14d
addshore <adamshorland@gmail.com>
2018-04-04 14:39:36 +0100

    Add test for LexemeContent::getPatchedCopy
    Change-Id: I15ec237d8fde2d40d766f1374d420e53892add7d

--- tests/phpunit/mediawiki/Content/LexemeContentTest.php
+++ tests/phpunit/mediawiki/Content/LexemeContentTest.php
@@ -2,6 +2,7 @@
 
 namespace Wikibase\Lexeme\Tests\MediaWiki\Content;
 
+use Diff\DiffOp\Diff\Diff;
 use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use PHPUnit4And6Compat;
@@ -12,8 +13,14 @@ use Wikibase\DataModel\Snak\PropertyNoValueSnak;
 use Wikibase\DataModel\Term\Term;
 use Wikibase\DataModel\Term\TermList;
 use Wikibase\Lexeme\Content\LexemeContent;
+use Wikibase\Lexeme\DataModel\FormId;
 use Wikibase\Lexeme\DataModel\Lexeme;
 use Wikibase\Lexeme\DataModel\LexemeId;
+use Wikibase\Lexeme\DataModel\Services\Diff\LexemeDiff;
+use Wikibase\Lexeme\DataModel\Services\Diff\LexemeDiffer;
+use Wikibase\Lexeme\Tests\DataModel\NewForm;
+use Wikibase\Lexeme\Tests\DataModel\NewLexeme;
+use Wikibase\Repo\Content\EntityContentDiff;
 
 /**
  * @covers \Wikibase\Lexeme\Content\LexemeContent
@@ -87,6 +94,78 @@ class LexemeContentTest extends TestCase {
 		yield [ new Lexeme( new LexemeId( 'L1' ), null, new ItemId( 'Q120' ), new ItemId( 'Q121' ) ) ];
 		yield [ new Lexeme( new LexemeId( 'L1' ), null, null, new ItemId( 'Q121' ) ) ];
 		yield [ new Lexeme( new LexemeId( 'L1' ), null, new ItemId( 'Q120' ) ) ];
+	}
+
+	/**
+	 * @dataProvider provideGetPatchedCopy
+	 */
+	public function testGetPatchedCopy( Lexeme $lexeme, LexemeDiff $lexemeDiff, $assertions ) {
+		$content = new LexemeContent( new EntityInstanceHolder( $lexeme ) );
+		$contentDiff = new EntityContentDiff( $lexemeDiff, new Diff( [] ), $lexeme->getType() );
+		$patchedCopy = $content->getPatchedCopy( $contentDiff );
+		$patchedEntity = $patchedCopy->getEntity();
+		$assertions( $lexeme, $patchedEntity );
+	}
+
+	public function provideGetPatchedCopy() {
+		$lexemeDiffer = new LexemeDiffer();
+
+		$newLexemeL1 = NewLexeme::havingId( 'L1' );
+		$newFormF1 = NewForm::havingId( new FormId( 'L1-F1' ) );
+
+		$newFormF1FeatureQ1 = $newFormF1->andGrammaticalFeature( new ItemId( 'Q1' ) );
+		$newFormF1FeatureQ2 = $newFormF1->andGrammaticalFeature( new ItemId( 'Q2' ) );
+		$newFormF1FeatureQ1andQ2 = $newFormF1->andGrammaticalFeature( new ItemId( 'Q1' ) )
+			->andGrammaticalFeature( new ItemId( 'Q2' ) );
+
+		// Build forms for use throughout that will have the same automatic representation added
+		$formF1FeatureQ1 = $newFormF1FeatureQ1->build();
+		$formF1FeatureQ2 = $newFormF1FeatureQ2->build();
+		$formF1FeatureQ1andQ2 = $newFormF1FeatureQ1andQ2->build();
+
+		$lexemeEmpty = $newLexemeL1->build();
+		$lexemeFormFeatureQ1 = $newLexemeL1->withForm( $formF1FeatureQ1 )->build();
+		$lexemeFormFeatureQ2 = $newLexemeL1->withForm( $formF1FeatureQ2 )->build();
+		$lexemeFormFeatureQ1andQ2 = $newLexemeL1->withForm( $formF1FeatureQ1andQ2 )->build();
+
+		yield 'Minimal entities, empty diff, should still be empty' => [
+			$lexemeEmpty,
+			$lexemeDiffer->diffEntities( $lexemeEmpty, $lexemeEmpty ),
+			function ( Lexeme $lexeme, Lexeme $lexemeCopy ) {
+				$this->assertTrue( $lexemeCopy->equals( $lexeme ) );
+			},
+		];
+		yield 'Entities with the same form, empty diff, should remain unchanged' => [
+			$lexemeFormFeatureQ1,
+			$lexemeDiffer->diffEntities( $lexemeFormFeatureQ1, $lexemeFormFeatureQ1 ),
+			function ( Lexeme $lexeme, Lexeme $lexemeCopy ) {
+				$this->assertTrue( $lexemeCopy->equals( $lexeme ) );
+			},
+		];
+		yield 'Adding a form feature (Q2)' => [
+			$lexemeFormFeatureQ1,
+			$lexemeDiffer->diffEntities( $lexemeFormFeatureQ1, $lexemeFormFeatureQ1andQ2 ),
+			function ( Lexeme $lexeme, Lexeme $lexemeCopy ) use ( $lexemeFormFeatureQ1andQ2 ) {
+				$this->assertFalse( $lexemeCopy->equals( $lexeme ) );
+				$this->assertTrue( $lexemeCopy->equals( $lexemeFormFeatureQ1andQ2 ) );
+			},
+		];
+		yield 'Removing a form feature (Q2)' => [
+			$lexemeFormFeatureQ1andQ2,
+			$lexemeDiffer->diffEntities( $lexemeFormFeatureQ1andQ2, $lexemeFormFeatureQ1 ),
+			function ( Lexeme $lexeme, Lexeme $lexemeCopy ) use ( $lexemeFormFeatureQ1 ) {
+				$this->assertFalse( $lexemeCopy->equals( $lexeme ) );
+				$this->assertTrue( $lexemeCopy->equals( $lexemeFormFeatureQ1 ) );
+			},
+		];
+		yield 'Changing a form feature (Q1 -> Q2)' => [
+			$lexemeFormFeatureQ1,
+			$lexemeDiffer->diffEntities( $lexemeFormFeatureQ1, $lexemeFormFeatureQ2 ),
+			function ( Lexeme $lexeme, Lexeme $lexemeCopy ) use ( $lexemeFormFeatureQ2 ) {
+				$this->assertFalse( $lexemeCopy->equals( $lexeme ) );
+				$this->assertTrue( $lexemeCopy->equals( $lexemeFormFeatureQ2 ) );
+			},
+		];
 	}
 
 }
