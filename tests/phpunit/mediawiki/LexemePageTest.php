bc31667bd314abd0f7f1b9da161e4c4ad633316d
Jakob Warkotsch <j.warkotsch@gmail.com>
2018-04-06 16:32:23 +0200

    Test deleting a Lexeme requires adequate permissions.
    Bug: T189482
    Change-Id: Ia11da5eee3f6c9418c6118c46becd43a6100d85b

---
+++ tests/phpunit/mediawiki/LexemePageTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Wikibase\Lexeme\Tests\MediaWiki;
+
+use Article;
+use User;
+use Wikibase\Lexeme\DataModel\Lexeme;
+use Wikibase\Lexeme\Tests\DataModel\NewLexeme;
+use Wikibase\Repo\WikibaseRepo;
+
+/**
+ * @license GPL-2.0-or-later
+ *
+ * @group Database
+ * @group medium
+ */
+class LexemePageTest extends WikibaseLexemeIntegrationTestCase {
+
+	public function testGivenUserHasNoDeleteRights_lexemePageCannotBeDeleted() {
+		$lexeme = $this->createTestLexeme( 'L123' );
+		$article = $this->newLexemePage( $lexeme );
+		$article->setContext( $this->newContextWithUser(
+			self::getTestUser( [] )->getUser() )
+		);
+
+		$this->setExpectedException( \PermissionsError::class );
+		$article->delete();
+	}
+
+	public function testGivenUserHasDeleteRights_lexemePageCanBeDeleted() {
+		$lexeme = $this->createTestLexeme( 'L123' );
+		$article = $this->newLexemePage( $lexeme );
+		$context = $this->newContextWithUser(
+			self::getTestUser( [ 'sysop' ] )->getUser()
+		);
+		$context->setOutput( new \OutputPage( new \RequestContext() ) );
+		$article->setContext( $context );
+
+		$article->delete();
+
+		$this->assertContains( 'Delete', $context->getOutput()->getPageTitle() );
+	}
+
+	private function createTestLexeme( $id ) {
+		$lexeme = NewLexeme::havingId( $id )->build();
+		WikibaseRepo::getDefaultInstance()
+			->getEntityStore()
+			->saveEntity( $lexeme, self::class, $this->getMock( \User::class ) );
+		return $lexeme;
+	}
+
+	private function newLexemePage( Lexeme $lexeme ) {
+		return new Article(
+			WikibaseRepo::getDefaultInstance()->getEntityTitleLookup()->getTitleForId( $lexeme->getId() )
+		);
+	}
+
+	private function newContextWithUser( User $user ) {
+		$context = new \RequestContext();
+		$context->setUser( $user );
+
+		return $context;
+	}
+
+}
