a5d09fd7914059130ca3d714a5f5834e07144dd3
Leszek Manicki <leszek.manicki@wikimedia.de>
2018-04-18 18:12:41 +0200

    Added test showing off issue in FormRevisionLookup
    Change-Id: Id47d348d7d3f1ad963fa272b68b7da7fb94b5327

---
+++ tests/phpunit/mediawiki/Store/LexemeStoreIntegrationTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Wikibase\Lexeme\Tests\Store;
+
+use User;
+use Wikibase\Lexeme\DataModel\FormId;
+use Wikibase\Lexeme\Tests\DataModel\NewForm;
+use Wikibase\Lexeme\Tests\DataModel\NewLexeme;
+use Wikibase\Lexeme\Tests\MediaWiki\WikibaseLexemeIntegrationTestCase;
+use Wikibase\Repo\WikibaseRepo;
+
+/**
+ * @license GPL-2.0-or-later
+ *
+ * @group Database
+ * @group medium
+ */
+class LexemeStoreIntegrationTest extends WikibaseLexemeIntegrationTestCase {
+
+	const LEXEME_ID = 'L1';
+	const FORM_ID = 'F1';
+	const FULL_FORM_ID = 'L1-F1';
+
+	public function testGivenExistingFormId_EntityLookupHasEntityReturnsTrue() {
+		$this->saveLexemeWithForm();
+
+		$lookup = WikibaseRepo::getDefaultInstance()->getEntityLookup();
+
+		$this->assertTrue( $lookup->hasEntity( new FormId( self::FULL_FORM_ID ) ) );
+	}
+
+	public function testGivenNotExistingFormId_EntityLookupHasEntityReturnsFalse() {
+		$this->markTestSkipped( 'Currently broken!' );
+
+		$this->saveLexemeWithoutForm();
+
+		$lookup = WikibaseRepo::getDefaultInstance()->getEntityLookup();
+
+		$this->assertFalse( $lookup->hasEntity( new FormId( self::FULL_FORM_ID ) ) );
+	}
+
+	private function saveLexemeWithForm() {
+		$store = WikibaseRepo::getDefaultInstance()->getEntityStore();
+
+		$lexeme = NewLexeme::havingId( self::LEXEME_ID )
+			->withForm(
+				NewForm::havingId( self::FORM_ID )->build()
+			)->build();
+
+		$store->saveEntity( $lexeme, self::class, $this->getMock( User::class ) );
+	}
+
+	private function saveLexemeWithoutForm() {
+		$store = WikibaseRepo::getDefaultInstance()->getEntityStore();
+
+		$lexeme = NewLexeme::havingId( self::LEXEME_ID )->build();
+
+		$store->saveEntity( $lexeme, self::class, $this->getMock( User::class ) );
+	}
+
+}
