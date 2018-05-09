eb186f76cd0fcf16cca18dd7e4bee6b73cb4e672
Leszek Manicki <leszek.manicki@wikimedia.de>
2018-05-09 16:49:13 +0200

    Added browser tests for editing form representations
    Change-Id: Ib2aa611cca6e15c225b349f3c5bac29a25fdf5f9

---
+++ tests/selenium/specs/form.edit.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const assert = require( 'assert' ),
+	LexemeApi = require( '../lexeme.api' ),
+	LexemePage = require( '../pageobjects/lexeme.page' );
+
+describe( 'Lexeme:Forms', () => {
+
+	it( 'can add representation', () => {
+		let id;
+
+		browser.call( () => {
+			return LexemeApi.create()
+				.then( ( lexeme ) => {
+					id = lexeme.id;
+				} )
+				.then( () => {
+					return LexemeApi.addForm(
+						id,
+						{
+							representations: {
+								'en-us': { language: 'en-us', value: 'color' }
+							},
+							grammaticalFeatures: []
+						}
+					);
+				} );
+		} );
+
+		LexemePage.open( id );
+
+		LexemePage.addRepresentationToNthForm( 0, 'colour', 'en-gb' );
+
+		browser.call( () => {
+			return LexemeApi.get( id )
+				.then( ( lexeme ) => {
+					assert.equal( 'color', lexeme.forms[ 0 ].representations[ 'en-us' ].value, 'Old representation not changed' );
+					assert.equal( 'colour', lexeme.forms[ 0 ].representations[ 'en-gb' ].value, 'New representation added' );
+				} );
+		} );
+
+	} );
+
+	it( 'can edit representation', () => {
+		let id;
+
+		browser.call( () => {
+			return LexemeApi.create()
+				.then( ( lexeme ) => {
+					id = lexeme.id;
+				} )
+				.then( () => {
+					return LexemeApi.addForm(
+						id,
+						{
+							representations: {
+								en: { language: 'en', value: 'color' }
+							},
+							grammaticalFeatures: []
+						}
+					);
+				} );
+		} );
+
+		LexemePage.open( id );
+
+		LexemePage.editRepresentationOfNthForm( 0, 'colour', 'en' );
+
+		browser.call( () => {
+			return LexemeApi.get( id )
+				.then( ( lexeme ) => {
+					assert.equal( 'colour', lexeme.forms[ 0 ].representations.en.value, 'Representation changed' );
+				} );
+		} );
+
+	} );
+
+	it( 'can remove representation', () => {
+		let id;
+
+		browser.call( () => {
+			return LexemeApi.create()
+				.then( ( lexeme ) => {
+					id = lexeme.id;
+				} )
+				.then( () => {
+					return LexemeApi.addForm(
+						id,
+						{
+							representations: {
+								'en-us': { language: 'en-us', value: 'color' },
+								'en-gb': { language: 'en-gb', value: 'colour' }
+							},
+							grammaticalFeatures: []
+						}
+					);
+				} );
+		} );
+
+		LexemePage.open( id );
+
+		LexemePage.removeLastRepresentationOfNthForm( 0 );
+
+		browser.call( () => {
+			return LexemeApi.get( id )
+				.then( ( lexeme ) => {
+					assert.ok( 'en-gb' in lexeme.forms[ 0 ].representations === false, 'Representation removed' );
+				} );
+		} );
+
+	} );
+
+} );
