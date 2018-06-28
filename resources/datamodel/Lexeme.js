a81cc2b226dd5074ca08e28a915f3a369d4c8b45
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2018-06-28 11:54:14 +0100

    Remove unused code from Lexeme frontend data model
    Remove unused features of the Lexeme:
     * language
     * lexical category
     * statements
     * equals()
     * isEmpty()
    
    Bug: T198379
    Change-Id: Id74b1234548533f4a831020e18ad58687cbb991a

--- resources/datamodel/Lexeme.js
+++ resources/datamodel/Lexeme.js
@@ -13,9 +13,6 @@
 	 *
 	 * @param {string} lexemeId
 	 * @param {wikibase.datamodel.TermMap} lemmas
-	 * @param {string} lexicalCategory
-	 * @param {string} language
-	 * @param {wikibase.datamodel.StatementGroupSet|null} [statementGroupSet=new wikibase.datamodel.StatementGroupSet()]
 	 * @param {wikibase.lexeme.datamodel.Form[]} [forms=[]]
 	 *
 	 * @throws {Error} if a required parameter is not specified properly.
@@ -23,17 +20,13 @@
 	var SELF = wb.lexeme.datamodel.Lexeme = util.inherit(
 		'WbDataModelLexeme',
 		PARENT,
-		function ( lexemeId, lemmas, lexicalCategory, language, statementGroupSet, forms ) {
-			statementGroupSet = statementGroupSet || new wb.datamodel.StatementGroupSet();
+		function ( lexemeId, lemmas, forms ) {
 			forms = forms || [];
 
 			if (
 				typeof lexemeId !== 'string' ||
 				!( lemmas instanceof wb.datamodel.TermMap ) ||
 				( lemmas.isEmpty() ) ||
-				typeof lexicalCategory !== 'string' ||
-				typeof language !== 'string' ||
-				!( statementGroupSet instanceof wb.datamodel.StatementGroupSet ) ||
 				!( Array.isArray( forms ) )
 			) {
 				throw new Error( 'Required parameter(s) missing or not defined properly' );
@@ -41,9 +34,6 @@
 
 			this._id = lexemeId;
 			this._lemmas = lemmas;
-			this._lexicalCategory = lexicalCategory;
-			this._language = language;
-			this._statementGroupSet = statementGroupSet;
 			this._forms = forms;
 		},
 		{
@@ -53,24 +43,6 @@
 			 * @private
 			 */
 			_lemmas: null,
-
-			/**
-			 * @property {string}
-			 * @private
-			 */
-			_lexicalCategory: null,
-
-			/**
-			 * @property {string}
-			 * @private
-			 */
-			_language: null,
-
-			/**
-			 * @property {wikibase.datamodel.StatementGroupSet}
-			 * @private
-			 */
-			_statementGroupSet: null,
 
 			/**
 			 * @property {wikibase.datamodel.Form[]}
@@ -86,66 +58,12 @@
 			},
 
 			/**
-			 * @return {string}
-			 */
-			getLexicalCategory: function () {
-				return this._lexicalCategory;
-			},
-
-			/**
-			 * @return {string}
-			 */
-			getLanguage: function () {
-				return this._language;
-			},
-
-			/**
-			 * @return {wikibase.datamodel.StatementGroupSet}
-			 */
-			getStatements: function () {
-				return this._statementGroupSet;
-			},
-
-			/**
 			 * @return {wikibase.lexeme.datamodel.Form[]}
 			 */
 			getForms: function () {
 				return this._forms;
-			},
-
-			/**
-			 * @return {boolean}
-			 */
-			isEmpty: function () {
-				return this._lemmas.isEmpty() &&
-					this._statementGroupSet.isEmpty() &&
-					this._forms.length === 0;
-			},
-
-			/**
-			 * @param {*} lexeme
-			 * @return {boolean}
-			 */
-			equals: function ( lexeme ) {
-				if ( this._forms.length !== lexeme._forms.length ) {
-					return false;
-				}
-
-				for ( var i = 0; i < this._forms.length; i++ ) {
-					if ( !this._forms[ i ].equals( lexeme._forms[ i ] ) ) {
-						return false;
-					}
-				}
-
-				return lexeme === this ||
-					( lexeme instanceof SELF &&
-						this._id === lexeme.getId() &&
-						this._lemmas.equals( lexeme.getLemmas() ) &&
-						this._statementGroupSet.equals( lexeme.getStatements() ) &&
-						this._lexicalCategory === lexeme.getLexicalCategory() &&
-						this._language === lexeme.getLanguage()
-					);
 			}
+
 		}
 	);
 
