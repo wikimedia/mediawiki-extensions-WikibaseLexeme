e397419cb582d02e0b7de3014f8197c806e161fb
addshore <addshorewiki@gmail.com>
2018-07-13 14:03:36 +0100

    Add parsing and deserialization of sense ChangeOps
    Bug: T198196
    Change-Id: I47d4cf9a963c88f1a7e915f3bfa664ae492dff95

---
+++ tests/phpunit/mediawiki/Api/AddSenseRequestParserTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Wikibase\Lexeme\Tests\MediaWiki\Api;
+
+use PHPUnit\Framework\TestCase;
+use Wikibase\DataModel\Deserializers\TermDeserializer;
+use Wikibase\DataModel\Entity\DispatchingEntityIdParser;
+use Wikibase\DataModel\Term\Term;
+use Wikibase\Lexeme\Api\AddSenseRequest;
+use Wikibase\Lexeme\Api\AddSenseRequestParser;
+use Wikibase\Lexeme\ChangeOp\ChangeOpGloss;
+use Wikibase\Lexeme\ChangeOp\ChangeOpGlossList;
+use Wikibase\Lexeme\ChangeOp\ChangeOpSenseAdd;
+use Wikibase\Lexeme\ChangeOp\ChangeOpSenseEdit;
+use Wikibase\Lexeme\ChangeOp\Deserialization\EditSenseChangeOpDeserializer;
+use Wikibase\Lexeme\ChangeOp\Deserialization\GlossesChangeOpDeserializer;
+use Wikibase\Lexeme\ChangeOp\Validation\LexemeTermLanguageValidator;
+use Wikibase\Lexeme\ChangeOp\Validation\LexemeTermSerializationValidator;
+use Wikibase\Lexeme\DataModel\LexemeId;
+use Wikibase\Lib\StaticContentLanguages;
+
+/**
+ * @covers \Wikibase\Lexeme\Api\AddSenseRequestParser
+ *
+ * @license GPL-2.0-or-later
+ */
+class AddSenseRequestParserTest extends TestCase {
+
+	public function testGivenValidData_parseReturnsRequest() {
+		$parser = $this->newAddSenseRequestParser();
+
+		$request = $parser->parse( [ 'lexemeId' => 'L1', 'data' => $this->getDataParam() ] );
+
+		$this->assertInstanceOf( AddSenseRequest::class, $request );
+	}
+
+	public function testLexemeIdPassedToRequestObject() {
+		$parser = $this->newAddSenseRequestParser();
+
+		$request = $parser->parse( [ 'lexemeId' => 'L1', 'data' => $this->getDataParam() ] );
+
+		$this->assertEquals( new LexemeId( 'L1' ), $request->getLexemeId() );
+	}
+
+	public function testSenseDataPassedToRequestObject() {
+		$parser = $this->newAddSenseRequestParser();
+
+		$request = $parser->parse( [ 'lexemeId' => 'L1', 'data' => $this->getDataParam() ] );
+
+		$this->assertEquals(
+			new ChangeOpSenseAdd( new ChangeOpSenseEdit( [
+				new ChangeOpGlossList( [ new ChangeOpGloss( new Term( 'en', 'furry animal' ) ) ] ),
+			] ) ),
+			$request->getChangeOp()
+		);
+	}
+
+	private function getDataParam( array $dataToUse = [] ) {
+		$simpleData = [
+			'glosses' => [
+				'en' => [
+					'language' => 'en',
+					'value' => 'furry animal',
+				],
+			],
+		];
+
+		return json_encode( array_merge( $simpleData, $dataToUse ) );
+	}
+
+	private function newAddSenseRequestParser() {
+		$idParser = new DispatchingEntityIdParser( [
+			LexemeId::PATTERN => function ( $id ) {
+				return new LexemeId( $id );
+			}
+		] );
+
+		$editSenseChangeOpDeserializer = new EditSenseChangeOpDeserializer(
+			new GlossesChangeOpDeserializer(
+				new TermDeserializer(),
+				new LexemeTermSerializationValidator(
+					new LexemeTermLanguageValidator( new StaticContentLanguages( [ 'en', 'de' ] ) )
+				)
+			)
+		);
+
+		return new AddSenseRequestParser(
+			$idParser,
+			$editSenseChangeOpDeserializer
+		);
+	}
+
+}
---
+++ tests/phpunit/mediawiki/ChangeOp/Deserialization/GlossesChangeOpDeserializerTest.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Wikibase\Lexeme\Tests\MediaWiki\ChangeOp\Deserialization;
+
+use ApiUsageException;
+use PHPUnit4And6Compat;
+use PHPUnit\Framework\TestCase;
+use Status;
+use Wikibase\DataModel\Deserializers\TermDeserializer;
+use Wikibase\DataModel\Term\Term;
+use Wikibase\Lexeme\ChangeOp\ChangeOpRemoveSenseGloss;
+use Wikibase\Lexeme\ChangeOp\ChangeOpGloss;
+use Wikibase\Lexeme\ChangeOp\ChangeOpGlossList;
+use Wikibase\Lexeme\ChangeOp\Deserialization\GlossesChangeOpDeserializer;
+use Wikibase\Lexeme\ChangeOp\Deserialization\ValidationContext;
+use Wikibase\Lexeme\ChangeOp\Validation\LexemeTermSerializationValidator;
+
+/**
+ * @covers \Wikibase\Lexeme\ChangeOp\Deserialization\GlossesChangeOpDeserializer
+ *
+ * @license GPL-2.0-or-later
+ */
+class GlossesChangeOpDeserializerTest extends TestCase {
+
+	use PHPUnit4And6Compat;
+
+	public function testCreateEntityChangeOpEmpty_yieldsZeroChangeOpGlossList() {
+		$glossDeserializer = $this->getMock( TermDeserializer::class );
+		$validator = $this->newDummyValidator();
+
+		$deserializer = new GlossesChangeOpDeserializer(
+			$glossDeserializer,
+			$validator
+		);
+		$changeOps = $deserializer->createEntityChangeOp( [] );
+
+		$this->assertInstanceOf( ChangeOpGlossList::class, $changeOps );
+		$this->assertCount( 0, $changeOps->getChangeOps() );
+	}
+
+	public function testCreateEntityChangeOpWithValidTerm_yieldsChangeOpGlossList() {
+		$glossDeserializer = $this->getMock( TermDeserializer::class );
+		$validator = $this->newDummyValidator();
+
+		$glossDeserializer
+			->expects( $this->once() )
+			->method( 'deserialize' )
+			->with( [ 'language' => 'en', 'value' => 'smth' ] )
+			->willReturn( new Term( 'en', 'smth' ) );
+
+		$deserializer = new GlossesChangeOpDeserializer(
+			$glossDeserializer,
+			$validator
+		);
+
+		$languageContext = $this->getContextSpy();
+		$glossContext = $this->getContextSpy();
+		$glossContext
+			->method( 'at' )
+			->with( 'en' )
+			->willReturn( $languageContext );
+		$deserializer->setContext( $glossContext );
+
+		$changeOps = $deserializer->createEntityChangeOp( [
+			'en' => [ 'language' => 'en', 'value' => 'smth' ]
+		] );
+
+		$this->assertInstanceOf( ChangeOpGlossList::class, $changeOps );
+		$this->assertCount( 1, $changeOps->getChangeOps() );
+		$this->assertInstanceOf( ChangeOpGloss::class, $changeOps->getChangeOps()[0] );
+	}
+
+	public function testCreateEntityChangeOpWithRemoval_yieldsChangeOpGlossList() {
+		$glossDeserializer = $this->getMock( TermDeserializer::class );
+		$validator = $this->newDummyValidator();
+
+		$deserializer = new GlossesChangeOpDeserializer(
+			$glossDeserializer,
+			$validator
+		);
+
+		$languageContext = $this->getContextSpy();
+		$glossContext = $this->getContextSpy();
+		$glossContext
+			->method( 'at' )
+			->with( 'en' )
+			->willReturn( $languageContext );
+		$deserializer->setContext( $glossContext );
+
+		$changeOps = $deserializer->createEntityChangeOp( [
+			'en' => [ 'language' => 'en', 'remove' => '' ]
+		] );
+
+		$this->assertInstanceOf( ChangeOpGlossList::class, $changeOps );
+		$this->assertCount( 1, $changeOps->getChangeOps() );
+		$this->assertInstanceOf( ChangeOpRemoveSenseGloss::class, $changeOps->getChangeOps()[0] );
+	}
+
+	/**
+	 * @expectedException \ApiUsageException
+	 */
+	public function testGivenChangeValidationFails_exceptionIsThrownInsteadOfCreatingChangeOp() {
+		$glossDeserializer = $this->getMock( TermDeserializer::class );
+		$validator = $this->newDummyValidator();
+		$validator->method( 'validate' )
+			->willThrowException(
+				new ApiUsageException( null, Status::newFatal( 'some-validation-error' ) )
+			);
+
+		$deserializer = new GlossesChangeOpDeserializer(
+			$glossDeserializer,
+			$validator
+		);
+
+		$languageContext = $this->getContextSpy();
+		$glossContext = $this->getContextSpy();
+		$glossContext
+			->method( 'at' )
+			->with( 'en' )
+			->willReturn( $languageContext );
+		$deserializer->setContext( $glossContext );
+
+		$deserializer->createEntityChangeOp( [
+			'en' => [ 'language' => 'en', 'value' => 'bad things' ]
+		] );
+	}
+
+	private function getContextSpy() {
+		return $this
+			->getMockBuilder( ValidationContext::class )
+			->disableOriginalConstructor()
+			->getMock();
+	}
+
+	/**
+	 * @return LexemeTermSerializationValidator|\PHPUnit_Framework_MockObject_MockObject
+	 */
+	private function newDummyValidator() {
+		$validator = $this->getMockBuilder( LexemeTermSerializationValidator::class )
+			->disableOriginalConstructor()
+			->getMock();
+		return $validator;
+	}
+
+}
