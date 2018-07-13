552984d23ba09960535a7a0839455a7d61b6a0c8
addshore <addshorewiki@gmail.com>
2018-07-13 14:03:36 +0100

    Rename FormSummaryAggregator to SummaryAggregator
    There is nothing form-specific in FormSummaryAggregator, and we need
    exactly the same thing for senses as well, so rename it to a more
    generic SummaryAggregator. Comments referencing forms or form elements
    are shuffled around and rephrased to become more generic and, hopefully,
    more understandable as well.
    
    Bug: T198196
    Change-Id: Ie5026d584e1b0d14432a967dfcbce3072a3abc4f

--- src/ChangeOp/ChangeOpRepresentationList.php
+++ src/ChangeOp/ChangeOpRepresentationList.php
@@ -4,7 +4,7 @@ namespace Wikibase\Lexeme\ChangeOp;
 
 use ValueValidators\Result;
 use Wikibase\DataModel\Entity\EntityDocument;
-use Wikibase\Lexeme\Api\Summary\FormSummaryAggregator;
+use Wikibase\Lexeme\Api\Summary\SummaryAggregator;
 use Wikibase\Lexeme\DataModel\Form;
 use Wikibase\Repo\ChangeOp\ChangeOp;
 use Wikibase\Repo\Store\EntityPermissionChecker;
@@ -24,7 +24,7 @@ class ChangeOpRepresentationList implements ChangeOp {
 	private $changeOps;
 
 	/**
-	 * @var FormSummaryAggregator
+	 * @var SummaryAggregator
 	 */
 	private $summaryAggregator;
 
@@ -33,7 +33,7 @@ class ChangeOpRepresentationList implements ChangeOp {
 	 */
 	public function __construct( array $changeOps ) {
 		$this->changeOps = $changeOps;
-		$this->summaryAggregator = new FormSummaryAggregator( self::SUMMARY_ACTION_AGGREGATE );
+		$this->summaryAggregator = new SummaryAggregator( self::SUMMARY_ACTION_AGGREGATE );
 	}
 
 	public function apply( EntityDocument $entity, Summary $summary = null ) {
