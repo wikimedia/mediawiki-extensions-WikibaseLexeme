2fcc6f2a386fbd387eba45cee11e3b3d3a6d4e2a
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-07-23 16:17:46 +0200

    Add two API errors for senses
    Bug: T199206
    Change-Id: I321bf7beb51e61cfbd7b1927ad3a2000dc99e16a

---
+++ src/Api/Error/ParameterIsNotSenseId.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Wikibase\Lexeme\Api\Error;
+
+use ApiMessage;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class ParameterIsNotSenseId implements ApiError {
+
+	/**
+	 * @var string
+	 */
+	private $given;
+
+	/**
+	 * @param string $given
+	 */
+	public function __construct( $given ) {
+		$this->given = $given;
+	}
+
+	/**
+	 * TODO senseId can occur as param (senseId) or in json (data). Clean generic $path solution?
+	 * Proposal: Unification of field and path (fields being first part of path no extra treatment)
+	 *
+	 * @see ApiError::asApiMessage()
+	 */
+	public function asApiMessage( $parameterName, array $path ) {
+		$message = new \Message(
+			'wikibaselexeme-api-error-parameter-not-sense-id',
+			[ $parameterName, implode( '/', $path ), json_encode( $this->given ) ]
+		);
+		return new ApiMessage( $message, 'bad-request' );
+	}
+
+}
