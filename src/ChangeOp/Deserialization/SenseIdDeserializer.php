df190aa37fc9d0f7cce3e3ebaedb1f90f6a82907
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-07-23 16:23:19 +0200

    Add SenseIdDeserializer
    Bug: T199206
    Change-Id: Ide2f3a195d72d6ce8c3a0fd311fbe1d394e6f14a

---
+++ src/ChangeOp/Deserialization/SenseIdDeserializer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Wikibase\Lexeme\ChangeOp\Deserialization;
+
+use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\DataModel\Entity\EntityIdParsingException;
+use Wikibase\Lexeme\Api\Error\ParameterIsNotSenseId;
+use Wikibase\Lexeme\DataModel\Sense;
+use Wikibase\Lexeme\DataModel\SenseId;
+
+/**
+ * A throwing ValidationContext guards us from actual null return values,
+ * w/o it the result is too fuzzy to regard it clean
+ *
+ * @license GPL-2.0-or-later
+ */
+class SenseIdDeserializer {
+
+	/**
+	 * @var EntityIdParser
+	 */
+	private $entityIdParser;
+
+	public function __construct( EntityIdParser $idParser ) {
+		$this->entityIdParser = $idParser;
+	}
+
+	/**
+	 * @param string $id
+	 * @param ValidationContext $validationContext
+	 * @return SenseId|null
+	 */
+	public function deserialize( $id, ValidationContext $validationContext ) {
+		try {
+			$senseId = $this->entityIdParser->parse( $id );
+		} catch ( EntityIdParsingException $e ) {
+			$validationContext->addViolation( new ParameterIsNotSenseId( $id ) );
+			return null;
+		}
+
+		if ( $senseId->getEntityType() !== Sense::ENTITY_TYPE ) {
+			$validationContext->addViolation( new ParameterIsNotSenseId( $id ) );
+			return null;
+		}
+
+		/** @var SenseId $senseId */
+		return $senseId;
+	}
+
+}
