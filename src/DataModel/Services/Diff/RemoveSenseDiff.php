a5809293f228bfc06cc5f0b195bb8c20aab3cec6
Amir Sarabadani <ladsgroup@gmail.com>
2018-07-31 16:01:28 +0200

    Introduce SenseDiff and its implementations
    Bug: T200795
    Change-Id: Ie8fc3606d7e3ed599fb7bd41bb363f405b00aba1

---
+++ src/DataModel/Services/Diff/RemoveSenseDiff.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Wikibase\Lexeme\DataModel\Services\Diff;
+
+use Diff\DiffOp\Diff\Diff;
+use LogicException;
+use Wikibase\Lexeme\DataModel\SenseId;
+
+/**
+ * @license GPL-2.0-or-later
+ */
+class RemoveSenseDiff implements SenseDiff {
+
+	/**
+	 * @var SenseId
+	 */
+	private $senseId;
+
+	/**
+	 * @var Diff
+	 */
+	private $diffOps;
+
+	public function __construct( SenseId $senseId, Diff $diffOps ) {
+		$this->senseId = $senseId;
+		$this->diffOps = $diffOps;
+	}
+
+	public function getRemovedSenseId() {
+		return $this->senseId;
+	}
+
+	/**
+	 * @return Diff
+	 */
+	public function getGlossesDiff() {
+		return isset( $this->diffOps['glosses'] ) ?
+			$this->diffOps['glosses']
+			: new Diff( [] );
+	}
+
+	/**
+	 * @return Diff
+	 */
+	public function getStatementsDiff() {
+		return isset( $this->diffOps['claim'] ) ?
+			$this->diffOps['claim']
+			: new Diff( [] );
+	}
+
+	public function serialize() {
+		throw new LogicException( "serialize() is not implemented" );
+	}
+
+	public function unserialize( $serialized ) {
+		throw new LogicException( "unserialize() is not implemented" );
+	}
+
+	public function getType() {
+		return 'diff';
+	}
+
+	public function isAtomic() {
+		return false;
+	}
+
+	public function toArray( $valueConverter = null ) {
+		throw new LogicException( 'toArray() is not implemented' );
+	}
+
+	public function count() {
+		return $this->diffOps->count();
+	}
+
+	public function getOperations() {
+		// Due to the way this DiffOp is structured the default implementation would return nothing
+		throw new LogicException( "getOperations() is not implemented" );
+	}
+
+	public function getArrayCopy() {
+		// Due to the way this DiffOp is structured the default implementation would return nothing
+		throw new LogicException( "getArrayCopy() is not implemented" );
+	}
+
+}
