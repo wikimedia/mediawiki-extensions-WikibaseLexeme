89bb95306c6998754b7a3e987e6cf2f7e2380447
Pablo Grass (WMDE) <pablo.grass@wikimedia.de>
2018-08-02 16:08:32 +0000

    Revert "Create a dummy item before running the first browser test."
    This reverts commit 847dba7ed4c88e73a5bd7f45cb77040044eaeca1.
    
    Change-Id: Ie6cd49616bdc8d308f47375c7dceb28ce64d9a3f

--- tests/selenium/specs/form.add.js
+++ tests/selenium/specs/form.add.js
@@ -2,30 +2,12 @@
 
 const assert = require( 'assert' ),
 	LexemeApi = require( '../lexeme.api' ),
-	LexemePage = require( '../pageobjects/lexeme.page' ),
-	WikibaseApi = require( '../../../../Wikibase/repo/tests/selenium/wikibase.api' );
+	LexemePage = require( '../pageobjects/lexeme.page' );
 
 describe( 'Lexeme:Forms', () => {
 
 	it( 'can be added', () => {
 		let id;
-
-		/**
-		 * This block is a hotfix for the integration issue with ContentTranslation
-		 * documented here:  https://phabricator.wikimedia.org/T200693
-		 *
-		 * Creating a dummy item and "handling" the failure is a temporary
-		 * workaround to the problem of not being able to create the first item.
-		 */
-		browser.call( () => {
-			return WikibaseApi.createItem()
-				.then( () => {
-					// do nothing
-				} )
-				.catch( () => {
-					// do nothing
-				} );
-		} );
 
 		browser.call( () => {
 			return LexemeApi.create()
