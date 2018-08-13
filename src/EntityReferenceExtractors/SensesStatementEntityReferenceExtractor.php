346113c9b730d3730d7d4a643d9f54fc4c66eec0
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2018-08-13 17:12:14 +0200

    Add statements from senses to Special:WhatLinksHere
    Bug: T200980
    Change-Id: If2bbde330995c2dfed4a50fc0b2a285181f75b5d

---
+++ src/EntityReferenceExtractors/SensesStatementEntityReferenceExtractor.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Wikibase\Lexeme\EntityReferenceExtractors;
+
+use Wikibase\DataModel\Entity\EntityDocument;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\Lexeme\DataModel\Lexeme;
+use Wikibase\Repo\EntityReferenceExtractors\EntityReferenceExtractor;
+use Wikibase\Repo\EntityReferenceExtractors\StatementEntityReferenceExtractor;
+use Wikimedia\Assert\Assert;
+
+/**
+ * Extracts the referenced entity ids of each lexeme's senses' statements
+ *
+ * @license GPL-2.0-or-later
+ */
+class SensesStatementEntityReferenceExtractor implements EntityReferenceExtractor {
+
+	/**
+	 * @var StatementEntityReferenceExtractor
+	 */
+	private $statementEntityReferenceExtractor;
+
+	public function __construct(
+		StatementEntityReferenceExtractor $statementEntityReferenceExtractor
+	) {
+		$this->statementEntityReferenceExtractor = $statementEntityReferenceExtractor;
+	}
+
+	/**
+	 * @param EntityDocument $lexeme
+	 * @return EntityId[]
+	 */
+	public function extractEntityIds( EntityDocument $lexeme ) {
+		Assert::parameterType( Lexeme::class, $lexeme, '$lexeme' );
+
+		$ids = [];
+
+		/** @var Lexeme $lexeme */
+		foreach ( $lexeme->getSenses()->toArray() as $sense ) {
+			$ids = array_merge(
+				$ids,
+				$this->statementEntityReferenceExtractor->extractEntityIds( $sense )
+			);
+		}
+
+		return array_unique( $ids );
+	}
+
+}
