ba3b0c9a6215a8ad224981367a5e03614cec87a0
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2018-08-28 12:00:32 +0200

    Use LatestRevisionIdResult in EntityRevisionLookup::getLatestRevisionId
    Only adding new interface compatibility to be able to merge it.
    All the old code will go away in the next patch.
    
    Bug: T112658
    Change-Id: I0d04e6e1d5da0f6dae1ad29e3dacbb12a9f2546a
    Depends-On: I942ddad926b8da4672294c18e79ab3a1aa73ea90

--- tests/phpunit/mediawiki/Store/SenseRevisionLookupTest.php
+++ tests/phpunit/mediawiki/Store/SenseRevisionLookupTest.php
@@ -12,6 +12,7 @@ use Wikibase\Lexeme\Tests\DataModel\NewLexeme;
 use Wikibase\Lexeme\Tests\DataModel\NewSense;
 use Wikibase\Lib\Store\EntityRevision;
 use Wikibase\Lib\Store\EntityRevisionLookup;
+use Wikibase\Lib\Store\LatestRevisionIdResult;
 use Wikimedia\Assert\ParameterTypeException;
 
 /**
@@ -121,6 +122,75 @@ class SenseRevisionLookupTest extends TestCase {
 					->withGloss( 'en', 'gloss' )
 			)
 			->build();
+	}
+
+	public function testGivenSenseId_getLatestRevisionIdCallsToParentServiceWithLexemeId_NEW() {
+		if ( !class_exists( LatestRevisionIdResult::class ) ) {
+			$this->markTestSkipped( 'LatestRevisionIdResult class doesn\'t exist' );
+		}
+
+		$defaultMode = EntityRevisionLookup::LATEST_FROM_REPLICA;
+		/** @var EntityRevisionLookup $parentService */
+		$parentService = $this->prophesize( EntityRevisionLookup::class );
+		$parentService->getLatestRevisionId( $this->lexemeId, $defaultMode )
+			->willReturn( LatestRevisionIdResult::concreteRevision( 123 ) );
+		$parentService->getEntityRevision( $this->lexemeId, 123, $defaultMode )->willReturn(
+			new EntityRevision( $this->newLexeme(), 123 )
+		);
+
+		$instance = new SenseRevisionLookup( $parentService->reveal() );
+
+		$result = $this->extractConcreteRevision(
+			$instance->getLatestRevisionId( $this->senseId )
+		);
+		$this->assertSame( 123, $result );
+	}
+
+	public function testLexemeDoesNotContainTheSense_getLatestRevisionIdReturnsNonexistentEntity_NEW(
+	) {
+		if ( !class_exists( LatestRevisionIdResult::class ) ) {
+			$this->markTestSkipped( 'LatestRevisionIdResult class doesn\'t exist' );
+		}
+
+		$defaultMode = EntityRevisionLookup::LATEST_FROM_REPLICA;
+
+		$parentService = $this->prophesize( EntityRevisionLookup::class );
+		$parentService->getLatestRevisionId( $this->lexemeId, $defaultMode )
+			->willReturn( LatestRevisionIdResult::concreteRevision( 123 ) );
+		$parentService->getEntityRevision( $this->lexemeId, 123, $defaultMode )->willReturn(
+			new EntityRevision( $this->newLexeme(), 123 )
+		);
+		$instance = new SenseRevisionLookup( $parentService->reveal() );
+
+		$this->assertNonexistentRevision(
+			$instance->getLatestRevisionId( new SenseId( 'L1-S200' ) )
+		);
+	}
+
+	private function extractConcreteRevision( LatestRevisionIdResult $result ) {
+		$shouldNotBeCalled = function () {
+			$this->fail( 'Not a concrete revision given' );
+		};
+
+		return $result->onNonexistentEntity( $shouldNotBeCalled )
+			->onRedirect( $shouldNotBeCalled )
+			->onConcreteRevision( 'intval' )
+			->map();
+	}
+
+	private function assertNonexistentRevision( LatestRevisionIdResult $result ) {
+		$shouldNotBeCalled = function () {
+			$this->fail( 'Not a nonexistent revision given' );
+		};
+
+		return $result->onRedirect( $shouldNotBeCalled )
+			->onConcreteRevision( $shouldNotBeCalled )
+			->onNonexistentEntity(
+				function () {
+					$this->assertTrue( true );
+				}
+			)
+			->map();
 	}
 
 }
