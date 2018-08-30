32f59e37ef7496c861b0644cb160c2c525547d9a
Jakob Warkotsch <j.warkotsch@gmail.com>
2018-08-30 17:21:54 +0200

    Fix merge error messages
    Change-Id: I8779e7b4f595662cd0afd8f2dcd35dc3681b3cdb

--- src/Merge/Exceptions/LexemeSaveFailedException.php
+++ src/Merge/Exceptions/LexemeSaveFailedException.php
@@ -10,7 +10,7 @@ use Message;
 class LexemeSaveFailedException extends MergingException {
 
 	public function getErrorMessage(): Message {
-		return new Message( 'wikibase-lexeme-mergelexemes-error-failed-modify' );
+		return new Message( 'wikibase-lexeme-mergelexemes-error-failed-save' );
 	}
 
 }
