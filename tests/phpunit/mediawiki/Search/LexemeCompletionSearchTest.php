260e8aa1ff7bcb4c3dafeabed4d830cf435ce293
Amir Sarabadani <ladsgroup@gmail.com>
2018-10-09 17:48:09 +0200

    Set serialize_precision to fix tests on PHP 7.1+
    See https://stackoverflow.com/questions/42981409
    
    Bug: T206100
    Depends-On: I5bdf3b7ba4d051b2b72446a7e45232ef82effad3
    Change-Id: I3e2de43575003e1285e5e7e87398fefe2288dd85
    (cherry picked from commit 206f7e377510dcdaf051362be52c98ab97469980)

--- tests/phpunit/mediawiki/Search/LexemeCompletionSearchTest.php
+++ tests/phpunit/mediawiki/Search/LexemeCompletionSearchTest.php
@@ -63,7 +63,12 @@ class LexemeCompletionSearchTest extends \MediaWikiTestCase {
 		);
 		$decodedQuery = json_decode( $elasticQuery, true );
 		unset( $decodedQuery['path'] );
+		// T206100
+		$serializePrecision = ini_get( 'serialize_precision' );
+		ini_set( 'serialize_precision', -1 );
 		$encodedData = json_encode( $decodedQuery, JSON_PRETTY_PRINT );
+		ini_set( 'serialize_precision', $serializePrecision );
+
 		$this->assertFileContains(
 			__DIR__ . "/../../data/lexemeCompletionSearch/$expected.expected",
 			$encodedData );
