c02611313ea8893c27aad35bd79276536672c4ba
Zoranzoki21 <zorandori4444@gmail.com>
2018-10-28 03:29:09 +0100

    Add aliases for Serbian
    Change-Id: I4666c07a1166b591382ce094dfa410fced3129d8

--- WikibaseLexeme.i18n.alias.php
+++ WikibaseLexeme.i18n.alias.php
@@ -6,8 +6,21 @@
  * @ingroup Extensions
  */
 $specialPageAliases = [];
+
 /** English (English) */
 $specialPageAliases['en'] = [
 	'NewLexeme' => [ 'NewLexeme' ],
 	'MergeLexemes' => [ 'MergeLexemes' ],
+];
+
+/** Serbian Cyrillic (српски (ћирилица)) */
+$specialPageAliases['sr-ec'] = [
+	'NewLexeme' => [ 'НоваЛексема' ],
+	'MergeLexemes' => [ 'СпојиЛексеме' ],
+];
+
+/** Serbian Latin (srpski (latinica) */
+	$specialPageAliases['sr-el'] = [
+	'NewLexeme' => [ 'NovaLeksema' ],
+	'MergeLexemes' => [ 'SpojiLekseme' ],
 ];
