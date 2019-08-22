6e7c4f265899ba71f00993cf3a9cf81556b19eca
Stanislav Malyshev <smalyshev@gmail.com>
2019-08-22 11:18:32 -0700

    Fix Lexemes RDF generation
    Bug: T230974
    Change-Id: I1129ae8f0d9e74143a0e360e215b6977c8dd7983

--- src/Presentation/Rdf/LexemeRdfBuilder.php
+++ src/Presentation/Rdf/LexemeRdfBuilder.php
@@ -85,34 +85,38 @@ class LexemeRdfBuilder implements EntityRdfBuilder {
 	 */
 	private function addLexeme( Lexeme $lexeme ) {
 		$lexemeLName = $this->vocabulary->getEntityLName( $lexeme->getId() );
+		$repositoryName = $this->vocabulary->getEntityRepositoryName( $lexeme->getId() );
+		$lexemePrefix = $this->vocabulary->entityNamespaceNames[$repositoryName];
 
-		$this->addLexemeTypes( $lexemeLName );
-		$this->addLemmas( $lexemeLName, $lexeme->getLemmas() );
-		$this->addLanguage( $lexemeLName, $lexeme->getLanguage() );
-		$this->addLexicalCategory( $lexemeLName, $lexeme->getLexicalCategory() );
-		$this->addForms( $lexemeLName, $lexeme->getForms() );
-		$this->addSenses( $lexemeLName, $lexeme->getSenses() );
+		$this->addLexemeTypes( $lexemePrefix, $lexemeLName );
+		$this->addLemmas( $lexemePrefix, $lexemeLName, $lexeme->getLemmas() );
+		$this->addLanguage( $lexemePrefix, $lexemeLName, $lexeme->getLanguage() );
+		$this->addLexicalCategory( $lexemePrefix, $lexemeLName, $lexeme->getLexicalCategory() );
+		$this->addForms( $lexemePrefix, $lexemeLName, $lexeme->getForms() );
+		$this->addSenses( $lexemePrefix, $lexemeLName, $lexeme->getSenses() );
 	}
 
 	/**
 	 * Adds the types of the given lexeme to the RDF graph
 	 *
+	 * @param string $lexemePrefix
 	 * @param string $lexemeLName
 	 */
-	private function addLexemeTypes( $lexemeLName ) {
-		$this->writer->about( RdfVocabulary::NS_ENTITY, $lexemeLName )
+	private function addLexemeTypes( $lexemePrefix, $lexemeLName ) {
+		$this->writer->about( $lexemePrefix, $lexemeLName )
 			->a( self::NS_ONTOLEX, 'LexicalEntry' );
 	}
 
 	/**
 	 * Adds the lemmas of the given lexeme to the RDF graph
 	 *
+	 * @param string $lexemePrefix
 	 * @param string $lexemeLName
 	 * @param TermList $lemmas
 	 */
-	private function addLemmas( $lexemeLName, TermList $lemmas ) {
+	private function addLemmas( $lexemePrefix, $lexemeLName, TermList $lemmas ) {
 		foreach ( $lemmas->toTextArray() as $lemmaCode => $lemmaText ) {
-			$this->writer->about( RdfVocabulary::NS_ENTITY, $lexemeLName )
+			$this->writer->about( $lexemePrefix, $lexemeLName )
 				->say( 'rdfs', 'label' )
 				->text( $lemmaText, $lemmaCode )
 				->say( RdfVocabulary::NS_ONTOLOGY, 'lemma' )
@@ -123,64 +127,70 @@ class LexemeRdfBuilder implements EntityRdfBuilder {
 	/**
 	 * Adds the language of the given lexeme to the RDF graph
 	 *
+	 * @param string $lexemePrefix
 	 * @param string $lexemeLName
 	 * @param ItemId $language
 	 */
-	private function addLanguage( $lexemeLName, ItemId $language ) {
+	private function addLanguage( $lexemePrefix, $lexemeLName, ItemId $language ) {
 		$languageLName = $this->vocabulary->getEntityLName( $language );
 		$this->entityMentionTracker->entityReferenceMentioned( $language );
+		$repositoryName = $this->vocabulary->getEntityRepositoryName( $language );
 
-		$this->writer->about( RdfVocabulary::NS_ENTITY, $lexemeLName )
+		$this->writer->about( $lexemePrefix, $lexemeLName )
 			->say( self::NS_DUBLIN_CORE_TERM, 'language' )
-			->is( RdfVocabulary::NS_ENTITY, $languageLName );
+			->is( $this->vocabulary->entityNamespaceNames[$repositoryName], $languageLName );
 	}
 
 	/**
 	 * Adds the lexical category of the given lexeme to the RDF graph
 	 *
+	 * @param string $lexemePrefix
 	 * @param string $lexemeLName
 	 * @param ItemId $lexicalCategory
 	 */
-	private function addLexicalCategory( $lexemeLName, ItemId $lexicalCategory ) {
+	private function addLexicalCategory( $lexemePrefix, $lexemeLName, ItemId $lexicalCategory ) {
 		$lexicalCategoryLName = $this->vocabulary->getEntityLName( $lexicalCategory );
 		$this->entityMentionTracker->entityReferenceMentioned( $lexicalCategory );
+		$repositoryName = $this->vocabulary->getEntityRepositoryName( $lexicalCategory );
 
-		$this->writer->about( RdfVocabulary::NS_ENTITY, $lexemeLName )
+		$this->writer->about( $lexemePrefix, $lexemeLName )
 			->say( RdfVocabulary::NS_ONTOLOGY, 'lexicalCategory' )
-			->is( RdfVocabulary::NS_ENTITY, $lexicalCategoryLName );
+			->is( $this->vocabulary->entityNamespaceNames[$repositoryName], $lexicalCategoryLName );
 	}
 
 	/**
 	 * Adds the forms of the given lexeme to the RDF graph
 	 *
+	 * @param string $lexemePrefix
 	 * @param string $lexemeLName
 	 * @param FormSet $forms
 	 */
-	private function addForms( $lexemeLName, FormSet $forms ) {
+	private function addForms( $lexemePrefix, $lexemeLName, FormSet $forms ) {
 		foreach ( $forms->toArray() as $form ) {
 			$this->entityMentionTracker->subEntityMentioned( $form );
 
 			$formLName = $this->vocabulary->getEntityLName( $form->getId() );
-			$this->writer->about( RdfVocabulary::NS_ENTITY, $lexemeLName )
+			$this->writer->about( $lexemePrefix, $lexemeLName )
 				->say( self::NS_ONTOLEX, 'lexicalForm' )
-				->is( RdfVocabulary::NS_ENTITY, $formLName );
+				->is( $lexemePrefix, $formLName );
 		}
 	}
 
 	/**
 	 * Adds the senses of the given lexeme to the RDF graph
 	 *
+	 * @param string $lexemePrefix
 	 * @param string $lexemeLName
 	 * @param SenseSet $senses
 	 */
-	private function addSenses( $lexemeLName, SenseSet $senses ) {
+	private function addSenses( $lexemePrefix, $lexemeLName, SenseSet $senses ) {
 		foreach ( $senses->toArray() as $sense ) {
 			$this->entityMentionTracker->subEntityMentioned( $sense );
 
 			$senseLName = $this->vocabulary->getEntityLName( $sense->getId() );
-			$this->writer->about( RdfVocabulary::NS_ENTITY, $lexemeLName )
+			$this->writer->about( $lexemePrefix, $lexemeLName )
 				->say( self::NS_ONTOLEX, 'sense' )
-				->is( RdfVocabulary::NS_ENTITY, $senseLName );
+				->is( $lexemePrefix, $senseLName );
 		}
 	}
 
@@ -191,31 +201,35 @@ class LexemeRdfBuilder implements EntityRdfBuilder {
 	 */
 	private function addForm( Form $form ) {
 		$formLName = $this->vocabulary->getEntityLName( $form->getId() );
+		$repositoryName = $this->vocabulary->getEntityRepositoryName( $form->getId() );
+		$lexemePrefix = $this->vocabulary->entityNamespaceNames[$repositoryName];
 
-		$this->addFormTypes( $formLName );
-		$this->addRepresentations( $formLName, $form->getRepresentations() );
-		$this->addGrammaticalFeatures( $formLName, $form->getGrammaticalFeatures() );
+		$this->addFormTypes( $lexemePrefix, $formLName );
+		$this->addRepresentations( $lexemePrefix, $formLName, $form->getRepresentations() );
+		$this->addGrammaticalFeatures( $lexemePrefix, $formLName, $form->getGrammaticalFeatures() );
 	}
 
 	/**
 	 * Adds the types of the given form to the RDF graph
 	 *
+	 * @param string $lexemePrefix
 	 * @param string $formLName
 	 */
-	private function addFormTypes( $formLName ) {
-		$this->writer->about( RdfVocabulary::NS_ENTITY, $formLName )
+	private function addFormTypes( $lexemePrefix, $formLName ) {
+		$this->writer->about( $lexemePrefix, $formLName )
 			->a( self::NS_ONTOLEX, 'Form' );
 	}
 
 	/**
 	 * Adds the representations of the given form to the RDF graph
 	 *
+	 * @param string $lexemePrefix
 	 * @param string $formLName
 	 * @param TermList $representations
 	 */
-	private function addRepresentations( $formLName, TermList $representations ) {
+	private function addRepresentations( $lexemePrefix, $formLName, TermList $representations ) {
 		foreach ( $representations->toTextArray() as $representationCode => $representationText ) {
-			$this->writer->about( RdfVocabulary::NS_ENTITY, $formLName )
+			$this->writer->about( $lexemePrefix, $formLName )
 				->say( 'rdfs', 'label' )
 				->text( $representationText, $representationCode )
 				->say( self::NS_ONTOLEX, 'representation' )
@@ -226,17 +240,19 @@ class LexemeRdfBuilder implements EntityRdfBuilder {
 	/**
 	 * Adds the grammatical features of the given form to the RDF graph
 	 *
+	 * @param string $lexemePrefix
 	 * @param string $formLName
 	 * @param ItemId[] $grammaticalFeatures
 	 */
-	private function addGrammaticalFeatures( $formLName, array $grammaticalFeatures ) {
+	private function addGrammaticalFeatures( $lexemePrefix, $formLName, array $grammaticalFeatures ) {
 		foreach ( $grammaticalFeatures as $grammaticalFeature ) {
 			$grammaticalFeatureLName = $this->vocabulary->getEntityLName( $grammaticalFeature );
 			$this->entityMentionTracker->entityReferenceMentioned( $grammaticalFeature );
+			$repositoryName = $this->vocabulary->getEntityRepositoryName( $grammaticalFeature );
 
-			$this->writer->about( RdfVocabulary::NS_ENTITY, $formLName )
+			$this->writer->about( $lexemePrefix, $formLName )
 				->say( RdfVocabulary::NS_ONTOLOGY, 'grammaticalFeature' )
-				->is( RdfVocabulary::NS_ENTITY, $grammaticalFeatureLName );
+				->is( $this->vocabulary->entityNamespaceNames[$repositoryName], $grammaticalFeatureLName );
 		}
 	}
 
@@ -247,30 +263,34 @@ class LexemeRdfBuilder implements EntityRdfBuilder {
 	 */
 	private function addSense( Sense $sense ) {
 		$senseLName = $this->vocabulary->getEntityLName( $sense->getId() );
+		$repositoryName = $this->vocabulary->getEntityRepositoryName( $sense->getId() );
+		$lexemePrefix = $this->vocabulary->entityNamespaceNames[$repositoryName];
 
-		$this->addSenseTypes( $senseLName );
-		$this->addGlosses( $senseLName, $sense->getGlosses() );
+		$this->addSenseTypes( $lexemePrefix, $senseLName );
+		$this->addGlosses( $lexemePrefix, $senseLName, $sense->getGlosses() );
 	}
 
 	/**
 	 * Adds the types of the given sense to the RDF graph
 	 *
+	 * @param string $lexemePrefix
 	 * @param string $senseLName
 	 */
-	private function addSenseTypes( $senseLName ) {
-		$this->writer->about( RdfVocabulary::NS_ENTITY, $senseLName )
+	private function addSenseTypes( $lexemePrefix, $senseLName ) {
+		$this->writer->about( $lexemePrefix, $senseLName )
 			->a( self::NS_ONTOLEX, 'LexicalSense' );
 	}
 
 	/**
 	 * Adds the glosses of the given sense to the RDF graph
 	 *
+	 * @param string $lexemePrefix
 	 * @param string $senseLName
 	 * @param TermList $glosses
 	 */
-	private function addGlosses( $senseLName, TermList $glosses ) {
+	private function addGlosses( $lexemePrefix, $senseLName, TermList $glosses ) {
 		foreach ( $glosses->toTextArray() as $glossCode => $glossText ) {
-			$this->writer->about( RdfVocabulary::NS_ENTITY, $senseLName )
+			$this->writer->about( $lexemePrefix, $senseLName )
 				->say( 'rdfs', 'label' )
 				->text( $glossText, $glossCode )
 				->say( RdfVocabulary::NS_SKOS, 'definition' )
@@ -304,9 +324,11 @@ class LexemeRdfBuilder implements EntityRdfBuilder {
 	 */
 	private function addLexemeSub( Lexeme $lexeme ) {
 		$lexemeLName = $this->vocabulary->getEntityLName( $lexeme->getId() );
+		$repositoryName = $this->vocabulary->getEntityRepositoryName( $lexeme->getId() );
+		$lexemePrefix = $this->vocabulary->entityNamespaceNames[$repositoryName];
 
-		$this->addLexemeTypes( $lexemeLName );
-		$this->addLemmas( $lexemeLName, $lexeme->getLemmas() );
+		$this->addLexemeTypes( $lexemePrefix, $lexemeLName );
+		$this->addLemmas( $lexemePrefix, $lexemeLName, $lexeme->getLemmas() );
 	}
 
 	/**
@@ -317,9 +339,11 @@ class LexemeRdfBuilder implements EntityRdfBuilder {
 	 */
 	private function addFormSub( Form $form ) {
 		$formLName = $this->vocabulary->getEntityLName( $form->getId() );
+		$repositoryName = $this->vocabulary->getEntityRepositoryName( $form->getId() );
+		$lexemePrefix = $this->vocabulary->entityNamespaceNames[$repositoryName];
 
-		$this->addFormTypes( $formLName );
-		$this->addRepresentations( $formLName, $form->getRepresentations() );
+		$this->addFormTypes( $lexemePrefix, $formLName );
+		$this->addRepresentations( $lexemePrefix, $formLName, $form->getRepresentations() );
 	}
 
 	/**
@@ -330,9 +354,11 @@ class LexemeRdfBuilder implements EntityRdfBuilder {
 	 */
 	private function addSenseSub( Sense $sense ) {
 		$senseLName = $this->vocabulary->getEntityLName( $sense->getId() );
+		$repositoryName = $this->vocabulary->getEntityRepositoryName( $sense->getId() );
+		$lexemePrefix = $this->vocabulary->entityNamespaceNames[$repositoryName];
 
-		$this->addSenseTypes( $senseLName );
-		$this->addGlosses( $senseLName, $sense->getGlosses() );
+		$this->addSenseTypes( $lexemePrefix, $senseLName );
+		$this->addGlosses( $lexemePrefix, $senseLName, $sense->getGlosses() );
 	}
 
 }
