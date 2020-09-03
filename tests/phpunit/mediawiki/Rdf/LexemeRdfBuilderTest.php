833d8145c3ec76ccc676a5cac848d98b5b2bd2cc
Pablo Grass <pablo.grass@wikimedia.de>
2020-09-03 18:13:13 +0200

    LexemeRdfBuilderTest: prepare for slimmer RdfBuilder
    I9045c3fc3100099f5f62b2cf933363c834d0bc16 will try and remove its first
    parameter.
    
    Change-Id: Ia9e5feecb47ca769b051ef736f0bcc173adddf46

--- tests/phpunit/mediawiki/Rdf/LexemeRdfBuilderTest.php
+++ tests/phpunit/mediawiki/Rdf/LexemeRdfBuilderTest.php
@@ -126,17 +126,32 @@ class LexemeRdfBuilderTest extends TestCase {
 		RdfWriter $writer, $produce, EntityTitleLookup $entityTitleLookup
 	) {
 		$wikibaseRepo = WikibaseRepo::getDefaultInstance();
-		$builder = new RdfBuilder(
-			$this->getTestData()->getSiteLookup()->getSites(),
-			$this->getVocabulary(),
-			$wikibaseRepo->getValueSnakRdfBuilderFactory(),
-			$this->getTestData()->getMockRepository(),
-			$wikibaseRepo->getEntityRdfBuilderFactory(),
-			$produce,
-			$writer,
-			new HashDedupeBag(),
-			$entityTitleLookup
-		);
+		$builderClass = new \ReflectionClass( 'Wikibase\Repo\Rdf\RdfBuilder' );
+		$builderParameters = $builderClass->getConstructor()->getParameters();
+		if ( $builderParameters[0]->getType()->getName() === \SiteList::class ) {
+			$builder = new RdfBuilder(
+				$this->getTestData()->getSiteLookup()->getSites(),
+				$this->getVocabulary(),
+				$wikibaseRepo->getValueSnakRdfBuilderFactory(),
+				$this->getTestData()->getMockRepository(),
+				$wikibaseRepo->getEntityRdfBuilderFactory(),
+				$produce,
+				$writer,
+				new HashDedupeBag(),
+				$entityTitleLookup
+			);
+		} else {
+			$builder = new RdfBuilder(
+				$this->getVocabulary(),
+				$wikibaseRepo->getValueSnakRdfBuilderFactory(),
+				$this->getTestData()->getMockRepository(),
+				$wikibaseRepo->getEntityRdfBuilderFactory(),
+				$produce,
+				$writer,
+				new HashDedupeBag(),
+				$entityTitleLookup
+			);
+		}
 		$builder->startDocument();
 		return $builder;
 	}
