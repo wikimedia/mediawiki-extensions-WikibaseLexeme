ce0782e5ff46b6750a629ae628bd89636c0c4f5e
Noa wmde <noa.rave@wikimedia.de>
2020-12-17 14:25:52 +0100

    WikibaseLexeme: add test comparing Vue versions with MW
    This test comapares the Vue and Vuex versions listed
    on our package.json with the Mediawiki version.
    Since we don't have Mediawiki access in CI this test uses
    the npm package require-from-url in order to load the
    vue file, so the version numbers may be extracted
    and inspected.
    In this case the test is located under mwlibs-testing
    directory in order to allow it its own eslint rules
    and avoid clashes with the main eslintrc.json
    
    Bug: T259468
    Change-Id: I7cd23af9bd169d0ce49ac4999a655c6f03d2d5bc

---
+++ tests/mwlibs-testing/mwlibs.js
@@ -0,0 +1,25 @@
+/**
+ * In production mode (on-wiki),
+ * some of our libraries are loaded as separate ResourceLoader modules,
+ * provided by MediaWiki core.
+ * This test checks that the versions provided by MediaWiki core
+ * are compatible with the requirements in our package.json.
+ */
+
+const assert = require( 'assert' );
+const process = require( 'process' );
+const semver = require( 'semver' );
+const requireFromUrl = require( 'require-from-url/sync' );
+const packageJson = require( '../../package.json' );
+
+const branch = process.env.ZUUL_BRANCH || 'master';
+
+const mwVue = requireFromUrl(
+	`https://raw.githubusercontent.com/wikimedia/mediawiki/${branch}/resources/lib/vue/vue.common.prod.js`
+);
+const mwVuex = requireFromUrl(
+	`https://raw.githubusercontent.com/wikimedia/mediawiki/${branch}/resources/lib/vuex/vuex.js`
+);
+
+assert( semver.satisfies( mwVue.version, packageJson.devDependencies.vue ) );
+assert( semver.satisfies( mwVuex.version, packageJson.devDependencies.vuex ) );
