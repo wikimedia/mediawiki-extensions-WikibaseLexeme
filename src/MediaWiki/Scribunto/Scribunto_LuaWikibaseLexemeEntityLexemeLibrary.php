3db446cbc2242411f97243787a32ed97f492679d
Michael Große <michael.grosse@wikimedia.de>
2021-07-21 15:28:25 +0200

    Use service to get UsageAccumulator
    Depends-On: I2cdb011374cbfb27c811ead56013b338541dd718
    Change-Id: I3ba2ce00c04089c5dc4f43f6452c49e1bb550dd0

--- src/MediaWiki/Scribunto/Scribunto_LuaWikibaseLexemeEntityLexemeLibrary.php
+++ src/MediaWiki/Scribunto/Scribunto_LuaWikibaseLexemeEntityLexemeLibrary.php
@@ -3,8 +3,6 @@
 namespace Wikibase\Lexeme\MediaWiki\Scribunto;
 
 use Scribunto_LuaLibraryBase;
-use Wikibase\Client\Usage\EntityUsageFactory;
-use Wikibase\Client\Usage\ParserOutputUsageAccumulator;
 use Wikibase\Client\Usage\UsageAccumulator;
 use Wikibase\Client\WikibaseClient;
 use Wikibase\DataModel\Entity\EntityIdParser;
@@ -23,10 +21,8 @@ class Scribunto_LuaWikibaseLexemeEntityLexemeLibrary extends Scribunto_LuaLibrar
 	public function getUsageAccumulator(): UsageAccumulator {
 		if ( $this->usageAccumulator === null ) {
 			$parserOutput = $this->getParser()->getOutput();
-			$this->usageAccumulator = new ParserOutputUsageAccumulator(
-				$parserOutput,
-				new EntityUsageFactory( $this->getEntityIdParser() )
-			);
+			$this->usageAccumulator = WikibaseClient::getUsageAccumulatorFactory()
+				->newFromParserOutput( $parserOutput );
 		}
 
 		return $this->usageAccumulator;
