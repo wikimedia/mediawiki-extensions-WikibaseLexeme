83b47f1d8dbf84095f426fc9b721bd8f118b3db1
Noa wmde <noa.rave@wikimedia.de>
2022-03-10 15:12:53 +0100

    Set up init() as the new app entrypoint
    init() is added to replace createAndMount() as the entry point to the
    new-lexeme app. Currently, Config and Mw are passed in as arguments.
    
    Bug: T302961
    Change-Id: I8c1b798887d530359bedfde5eb3f6555ae5d723e

--- resources/special/NewLexemeAlpha.js
+++ resources/special/NewLexemeAlpha.js
@@ -1,23 +1,14 @@
 ( function () {
-	var createAndMount = require( './new-lexeme-dist/SpecialNewLexeme.cjs.js' );
+	var init = require( './new-lexeme-dist/SpecialNewLexeme.cjs.js' );
 	var config = require( './licenseConfig.json' );
 
-	var messagesRepository = {
-		get: function () {
-			return mw.message.apply( mw.message, arguments ).parse();
-		},
-		getText: function () {
-			return mw.message.apply( mw.message, arguments ).text();
-		}
-	};
-
-	createAndMount(
+	init(
 		{
 			rootSelector: '#special-newlexeme-root',
 			token: mw.user.tokens.get( 'csrfToken' ),
 			licenseUrl: config.licenseUrl,
 			licenseName: config.licenseText
 		},
-		messagesRepository
+		mw
 	);
 }() );
