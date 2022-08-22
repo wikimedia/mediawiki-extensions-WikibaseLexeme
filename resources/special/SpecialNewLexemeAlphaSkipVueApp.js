292d5c27d910de04682a636413bf816588f49163
Noa wmde <noa.rave@wikimedia.de>
2022-08-22 09:21:04 +0200

    Lexidata: change IE11 nojs support to es5
    This is a follow up for change 824504, where
    es6 was used although IE11 does not support it
    (doh!)
    
    Bug: T311157
    Change-Id: Id9747e497155a253a98a844a6eb10383b16225ab

--- resources/special/SpecialNewLexemeAlphaSkipVueApp.js
+++ resources/special/SpecialNewLexemeAlphaSkipVueApp.js
@@ -2,7 +2,7 @@ if ( mw.loader.moduleRegistry['wikibase.lexeme.special.NewLexemeAlpha'] ) {
 	return false;
 }
 
-const root = document.querySelector('#special-newlexeme-root');
-const noscript = document.querySelector('#special-newlexeme-noscript');
+var root = document.querySelector('#special-newlexeme-root');
+var noscript = document.querySelector('#special-newlexeme-noscript');
 root.innerHTML = noscript.textContent;
 return true;
