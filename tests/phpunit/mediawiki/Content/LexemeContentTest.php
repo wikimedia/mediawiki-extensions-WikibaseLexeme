2c641b576153c3d4c2c16f1b78954492c89f8f89
Lucas Werkmeister (WMDE) <lucas.werkmeister@wikimedia.de>
2023-11-01 11:41:33 +0000

    Revert "Page properties should always be strings"
    This was breaking our monitoring by making the pp_sortkey always null
    rather than the number of statements, forms and senses.
    
    The MediaWiki effort that originally motivated the change, making all
    page properties into strings (T305158), appears to have stalled –
    there’s been no activity on it since my comment in April 2022. (In
    hindsight, that comment came frustratingly close to realizing this
    issue, but in the end we only noticed it 19 months later.) If it ever
    gets unstuck, we can consider this again, but we’ll need some solution
    to keep the numeric pp_sortkey.
    
    This reverts commit 253ede8d3ff6909c7fb8024f144fd2934b0e6cb0.
    
    Bug: T350224
    Change-Id: I5d7cb7f61ff1d04b60df6429326e59531786df59

--- tests/phpunit/mediawiki/Content/LexemeContentTest.php
+++ tests/phpunit/mediawiki/Content/LexemeContentTest.php
@@ -251,7 +251,7 @@ class LexemeContentTest extends MediaWikiLangTestCase {
 
 		$pageProps = $content->getEntityPageProperties();
 
-		$this->assertSame( $expectedCount, (int)$pageProps['wb-claims'] );
+		$this->assertSame( $expectedCount, $pageProps['wb-claims'] );
 	}
 
 	public function provideLexemesWithStatementCount() {
@@ -293,8 +293,8 @@ class LexemeContentTest extends MediaWikiLangTestCase {
 
 		$pageProps = $content->getEntityPageProperties();
 
-		$this->assertSame( $expectedSensesCount, (int)$pageProps['wbl-senses'] );
-		$this->assertSame( $expectedFormsCount, (int)$pageProps['wbl-forms'] );
+		$this->assertSame( $expectedSensesCount, $pageProps['wbl-senses'] );
+		$this->assertSame( $expectedFormsCount, $pageProps['wbl-forms'] );
 	}
 
 	public function provideLexemesWithSensesAndForms() {
