aa8ebc223b7f3a0f591850d1c6627b3c3986605a
Arthur Taylor <arthur.taylor@wikimedia.de>
2024-08-16 14:36:50 +0200

    Port selemium 'special/new.js' new Lemexe specs to cypress
    As part of the migration from Selenium to Lexeme, port the New
    Lexeme `can create lexeme` spec to cypress.
    
    This change introduces a dependency on the `cypress-wikibase-api`
    library, which is a new Javascript library that collects utility
    functions and cypress tasks to support working with the Wikibase
    API.
    
    Bug: T372139
    Change-Id: I7f78654b53c3680a8007c53c96ee19196ab1cdd9

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from "cypress";
 
 const envLogDir = process.env.LOG_DIR ? process.env.LOG_DIR + '/WikibaseLexeme' : null
+
+if ( process.env.MW_SERVER === undefined || process.env.MW_SCRIPT_PATH === undefined ||
+     process.env.MEDIAWIKI_USER === undefined || process.env.MEDIAWIKI_PASSWORD === undefined ) {
+	throw new Error( "Please define MW_SERVER, MW_SCRIPT_PATH, " +
+		"MEDIAWIKI_USER and MEDIAWIKI_PASSWORD environment variables" );
+}
+process.env.REST_BASE_URL = process.env.MW_SERVER + process.env.MW_SCRIPT_PATH + '/';
+
+import { mwApiCommands } from 'cypress-wikibase-api';
+
 export default defineConfig({
 	e2e: {
 		supportFile: false,
-		baseUrl: process.env.MW_SERVER + process.env.MW_SCRIPT_PATH
+		baseUrl: process.env.MW_SERVER + process.env.MW_SCRIPT_PATH,
+		mediawikiAdminUsername: process.env.MEDIAWIKI_USER,
+		mediawikiAdminPassword: process.env.MEDIAWIKI_PASSWORD,
+		setupNodeEvents( on, config ) {
+			on( 'task', {
+				...mwApiCommands( config ),
+			} );
+		},
 	},
 	screenshotsFolder: envLogDir || 'cypress/screenshots',
 	videosFolder: envLogDir || 'cypress/videos',
---
+++ cypress/e2e/special/newLexeme.cy.ts
@@ -0,0 +1,44 @@
+import { NewLexemePage } from '../../support/pageObjects/NewLexemePage'
+import { LexemePage } from '../../support/pageObjects/LexemePage'
+import { Util } from 'cypress-wikibase-api'
+
+const newLexemePage = new NewLexemePage()
+const lexemePage = new LexemePage()
+
+describe( 'NewLexeme:Page', () => {
+
+    it( 'request with "createpage" right shows form', () => {
+        newLexemePage.open()
+        newLexemePage.showsForm()
+    } )
+
+    it( 'can create lexeme', () => {
+        const lemma = Util.getTestString( 'lemma-' ),
+            language = Util.getTestString( 'language-' ),
+            languageItemsLanguageCode = 'aae',
+            lexicalCategory = Util.getTestString( 'lexicalCategory-' );
+        newLexemePage.open()
+
+        cy.task( 'MwApi:CreateItem', { label: language } ).then( languageId => {
+            cy.task( 'MwApi:CreateItem', { label: lexicalCategory } ).then( lexicalCategoryId => {
+                newLexemePage.createLexeme(
+                    lemma,
+                    languageId,
+                    lexicalCategoryId,
+                    languageItemsLanguageCode
+                );
+
+                lexemePage.lemmaContainer()
+
+                lexemePage.getHeaderId().then( ( lexemeId ) => {
+					return cy.task('MwApi:GetEntityData', {entityId: lexemeId});
+				} ).then( ( lexeme ) => {
+					expect( lexeme.lemmas[ languageItemsLanguageCode ].value ).to.eq( lemma );
+					expect( lexeme.language ).to.eq( languageId );
+					expect( lexeme.lexicalCategory ).to.eq( lexicalCategoryId );
+				} );
+            } )
+        } );
+    } )
+
+} )
